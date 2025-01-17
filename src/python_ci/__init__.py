fbb676f7734b33f73d45a14d729516da801cd41f
AlexeyZh <waltafunk@gmail.com>
2025-01-17 21:34:56 +0300

    feat: added simple function

---
+++ src/python_ci/__init__.py
@@ -0,0 +1,11 @@
+# pylint: disable=missing-module-docstring
+
+
+def main() -> None:
+    "The main function prints the string 'Hello from python-ci!' to the console."
+    print("Hello from python-ci!")
+
+
+def generate_list() -> int:
+    "This function returns 42."
+    return 42
